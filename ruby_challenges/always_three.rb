a798212bcef99aab4dcf0b7e608f50ebcc60c992
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-02-22 11:01:40 -0500

    Numerology.rb

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -1,8 +1,12 @@
+# puts "Give me a number."
+# number1 = Integer(gets)
+# number2 = number1 + 5
+# number2 *= 2
+# number2 -= 4
+# number2 /= 2
+# number2 -= number1
+# puts number2
+
 puts "Give me a number."
-number1 = Integer(gets)
-number2 = number1 + 5
-number2 *= 2
-number2 -= 4
-number2 /= 2
-number2 -= number1
-puts number2
+my_number = (gets.to_i + 5)(2) - 4
+puts my_number
