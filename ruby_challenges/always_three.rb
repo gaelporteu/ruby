8df9f6c85ac0340f99f2e1414e07e3636a6929fd
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-02-27 16:36:46 -0500

    Always three method

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -7,10 +7,12 @@
 # number2 -= number1
 # puts number2
 
-def always_three
-    puts "Give me a number."
+
+def always_three(number)
     number = Integer(gets) 
     puts number2 = (((number + 5) * 2) - 4) / 2 - number
 end
 
-always_three
+always_three(
+    puts "Give me a number." 
+    )
