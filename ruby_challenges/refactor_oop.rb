9692734ce21f91b7aeea42b73c897f4e855e729d
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-04-02 22:57:42 -0400

    Changes to refactor_oop.rb still not working

--- ruby_challenges/refactor_oop.rb
+++ ruby_challenges/refactor_oop.rb
@@ -2,27 +2,27 @@
 class Animal
     # no spaces between = 
 	def set_name=(animal_name)
-		@name = animal_name
+		@animal_name = animal_name
 	end
  
 	def get_name
-		return @name
+		return @animal_name
 	end
-    # it is always @name for the setter and getter
+    
 	def set_owner=(owner_name)
-		@name = owner_name
+		@owner_name = owner_name
 	end
-    # it is always @name for the setter and getter
+    
 	def get_owner
-		return @name
+		return @owner_name
 	end
     
     def set_noise=(animal_noise)
-		@name = animal_noise
+		@animal_noise = animal_noise
 	end
 
 	def get_noise
-		return @name
+		return @animal_noise
 	end
 
 end
@@ -30,26 +30,26 @@ end
 
  
 my_ferret = Animal.new
-my_ferret.set_name= "Fredo"
-animal_name = my_ferret.get_name
+my_ferret.set_animal_name = "Fredo"
+animal_name = my_ferret.get_animal_name
 my_ferret.set_noise = "squeee"
 animal_noise = my_ferret.get_noise
  
 my_parrot = Animal.new
-my_parrot.set_name= "Budgie"
-animal_name = my_parrot.get_name
+my_parrot.set_animal_name = "Budgie"
+animal_name = my_parrot.get_animal_name
 my_parrot.set_noise = "tweet tweet"
 animal_noise = my_parrot.get_noise
  
 my_chincilla = Animal.new
-my_chincilla.set_name= "Dali"
-animal_name = my_chincilla.get_name
+my_chincilla.set_animal_name = "Dali"
+animal_name = my_chincilla.get_animal_name
 my_chincilla.set_noise = "eeeeeeeq"
 animal_noise = my_chincilla.get_noise
  
-puts "#{my_ferret.name} says #{my_ferret.noise}, 
-#{my_parrot.name} says #{my_parrot.noise}, 
-and #{my_chincilla.name} says #{my_chincilla.noise}."
+puts "#{my_ferret.animal_name} says #{my_ferret.animal_noise}, 
+#{my_parrot.animal_name} says #{my_parrot.animal_noise}, 
+and #{my_chincilla.animal_name} says #{my_chincilla.animal_noise}."
  
 puts my_ferret.inspect
 puts my_parrot.inspect
